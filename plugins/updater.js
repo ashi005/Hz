e48ccb57101517b4afc86b8b10ba97b0954325d0
vbxngvklfhgikklvfsvrfundfnosdif <109337471+vbxngvklfhgikklvfsvrfundfnosdif@users.noreply.github.com>
2022-07-22 16:39:44 +0530

    H

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,80 +1,50 @@
-/* Copyright (C) 2021 TENUX-Neotro.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
-*/
+
+const cosec = require('../cosec');
+const Cosec = require('../cosec');
+const Config = require('../config');
+const config = require('../config');
+const diana = require("../events");
+const {
+	default: makeWASocket,
+	useSingleFileAuthState,
+	DisconnectReason,
+	getContentType
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+const P = require('pino')
 
 const simpleGit = require('simple-git');
 const git = simpleGit();
-const amazone = require('../events');
-const {MessageType,Mimetype} = require('@adiwajshing/baileys');
-const cosec = require('../cosec');
-const Config = require('../config');
 const exec = require('child_process').exec;
 const Heroku = require('heroku-client');
 const { PassThrough } = require('stream');
 const heroku = new Heroku({ token: cosec.HEROKU.API_KEY })
-const axios = require('axios');
+const { state, saveState } = useSingleFileAuthState('./session.json')
 const Language = require('../language');
 const Lang = Language.getString('updater');
+			       
 
-//උස්සන උන්ගෙ රෙපෝ කෙලවිලාම පලයම්
-var logoimage = new Array ();
+  
+		
 
-logoimage[0] = "https://telegra.ph/file/a51a91c22de055d730c6e.jpg";
-logoimage[1] = "https://telegra.ph/file/34ef0c8009b176cd5772d.jpg";
-logoimage[2] = "https://telegra.ph/file/75433f8d2a4523e14a966.jpg";
-logoimage[3] = "https://telegra.ph/file/b61382ac648c3957a62af.jpg";
-logoimage[4] = "https://telegra.ph/file/a51a91c22de055d730c6e.jpg";
+diana.addCommand({pattern: 'update now$', fromMe: true, deleteCommand: false, desc: "ping pong 📨"}, (async (message, match) => {
 
-  var i = Math.floor(5*Math.random())
-
-  var uplogonew = logoimage[i]    
-
-amazone.addCommand({pattern: 'update$', fromMe: true,  desc: Lang.UPDATER_DESC}, (async (message, match) => {
-    await git.fetch();
+ 
+				
+					 await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
-        
-        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: '\n\n\n\n' + Lang.UPDATE +'\n\n\n\n *👩‍🦰powerd by amazone Alexa*' })
-    
+      //  reply('no updates')  
+await  message.client.sendMessage(message.jid , { text: Lang.UPDATE }, { quoted: message.data } )
+  
     } else {
-        var newzels = Lang.NEW_UPDATE;
-        commits['all'].map(
-            (commit) => {
-                newzels += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
-            }
-        );
-        
-        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: newzels + '```'+'\n\n *👩‍🦰powerd by amazone Alexa*' })
-        
-    }
- }));  
-
-
-
-amazone.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
-    await git.fetch();
-    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
-    if (commits.total === 0) {
-        return await message.client.sendMessage(
-            message.jid,
-            Lang.UPDATE, MessageType.text
-        );    
-    } else {
-        var guncelleme = await message.reply(Lang.UPDATING);
-        if (cosec.HEROKU.HEROKU) {
+    if (cosec.HEROKU.HEROKU) {
             try {
                 var app = await heroku.get('/apps/' + cosec.HEROKU.APP_NAME)
             } catch {
-                await message.client.sendMessage(
-                    message.jid,Lang.INVALID_HEROKU, MessageType.text);
-                await new Promise(r => setTimeout(r, 1000));
-                return await message.client.sendMessage(
-                    message.jid,Lang.IN_AF, MessageType.text);
+                message.client.sendMessage(message.jid , { text: Lang.INVALID_HEROKU + "\n\n" + IN_AF }, { quoted: message.data } );
             }
+            message.client.sendMessage(message.jid , { text: Lang.UPDATING }, { quoted: message.data } );
 
             git.fetch('upstream', Config.BRANCH);
             git.reset('hard', ['FETCH_HEAD']);
@@ -88,23 +58,32 @@ amazone.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_
             } catch { console.log('heroku remote ekli'); }
             await git.push('heroku', Config.BRANCH);
 
-            await message.client.sendMessage(
-                message.jid,Lang.UPDATED, MessageType.text);
+            message.client.sendMessage(message.jid , { text: Lang.UPDATED_LOCAL }, { quoted: message.data } );
+            message.client.sendMessage(message.jid , { text: Lang.AFTER_UPDATE }, { quoted: message.data } );
 
-            await message.sendMessage(Lang.AFTER_UPDATE);
-            
-        } else {
-            git.pull((async (err, update) => {
-                if(update && update.summary.changes) {
-                    await message.client.sendMessage(
-                        message.jid,Lang.UPDATED_LOCAL, MessageType.text);
-                    exec('npm install').stderr.pipe(process.stderr);
-                } else if (err) {
-                    await message.client.sendMessage(
-                        message.jid,'*❌ Have a Error write our support group get more help*\n*Hata:* ```' + err + '```', MessageType.text);
-                }
-            }));
-            await guncelleme.delete();
-        }
+        } 
     }
+}));
+//===============================================CHECK UPDATE=========================================					
+	diana.addCommand({pattern: 'update$', fromMe: true, deleteCommand: false, desc: "ping pong 📨"}, (async (message, match) => {
+			
+					 await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+       // reply('no updates')    
+await  message.client.sendMessage(message.jid , { text: Lang.UPDATE }, { quoted: message.data } )
+    } else {
+
+        var newzels = Lang.NEW_UPDATE ;
+        commits['all'].map(
+            (commit) => {
+                newzels += '\n\n' +'🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
+            }
+        );
+      //  reply(ne
+await  message.client.sendMessage(message.jid , { text: newzels }, { quoted: message.data } )
+
+    }
+
+
 }));
